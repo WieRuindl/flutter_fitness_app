a88aaa99d5ab84d0ee0d558cbc3397522c80c46d
bepa <pavel.belov@zuhlke.com>
2024-05-15 16:20:14 +0300

    separated main screen to 3 different areas

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class MainScreen extends StatelessWidget {
@@ -5,6 +6,26 @@ class MainScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: Row(
+          children: [
+            Expanded(
+              flex: 2,
+              child: Container(color: Colors.red,),
+            ),
+            Expanded(
+              flex: 7,
+              child: Container(color: Colors.blue,),
+            ),
+            Expanded(
+              flex: 3,
+              child: Container(color: Colors.green,),
+            ),
+          ],
+        ),
+
+      ),
+    );
   }
 }
